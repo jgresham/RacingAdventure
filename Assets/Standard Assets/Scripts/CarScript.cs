f783aa59cb0a1c91e91d2081c3c6da51b20eb277
Joseph <joehuntosu@gmail.com>
2015-04-16 14:15:23 -0400

    added car to track

---
+++ Assets/Standard Assets/Scripts/CarScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarScript : MonoBehaviour {
+
+	public float speed;
+	public float rotationSpeed;
+	float translation;
+	float rotation;
+
+	// Use this for initialization
+	void Start () {
+		speed = 25.0f;
+		rotationSpeed = 100.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		translation = Input.GetAxis ("Vertical") * speed;
+		rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotation, 0);
+	}
+}
