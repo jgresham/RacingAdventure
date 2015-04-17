b67cc699a1666de1ce40ae4d561e5b40d9376c10
Johns Gresham <johnsgresham@gmail.com>
2015-04-17 11:57:29 -0400

    testied car with gravity

---
+++ Assets/Scripts/WheelScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelScript : MonoBehaviour {
+
+	WheelCollider WheelLF, WheelLR, WheelRF, WheelRR;
+	float maxTorque = 50;
+
+	// Use this for initialization
+	void Start () {
+		WheelLF = transform.FindChild("WheelColliders").FindChild("Wheel_LF").GetComponent<WheelCollider>();
+		WheelLR = transform.FindChild("WheelColliders").FindChild("Wheel_LR").GetComponent<WheelCollider>(); 
+		WheelRF = transform.FindChild("WheelColliders").FindChild("Wheel_RF").GetComponent<WheelCollider>(); 
+		WheelRR = transform.FindChild("WheelColliders").FindChild("Wheel_RR").GetComponent<WheelCollider>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		WheelRR.motorTorque = -maxTorque * Input.GetAxis ("Vertical");
+		WheelLR.motorTorque = -maxTorque * Input.GetAxis ("Vertical");
+		WheelLF.steerAngle = 10 * Input.GetAxis ("Horizontal");
+		WheelRF.steerAngle = 10 * Input.GetAxis ("Horizontal");
+	}
+}
