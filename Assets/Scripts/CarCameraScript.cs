088da802854a59d75abda37c2427fae1d0d4205e
Johns Gresham <johnsgresham@gmail.com>
2015-04-20 11:59:16 -0400

    wheel rotation and cool camera follow/backup

---
+++ Assets/Scripts/CarCameraScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarCameraScript : MonoBehaviour {
+
+	public Transform Car;
+	public float distance = 5.4f, height = 1.4f, rotationDamping = 3.0f, heightDamping = 2.0f, zoomRatio = 10.0f;
+	private Vector3 rotationVector = new Vector3();
+	public float localVelocity;
+
+	public float defaultFOV = 60; 
+
+	void LateUpdate () {
+		float wantedAngle = rotationVector.y;
+		float wantedHeight = Car.position.y + height;
+		float myAngle = transform.eulerAngles.y;
+		float myHeight = transform.position.y;
+
+		myAngle = Mathf.LerpAngle (myAngle, wantedAngle, rotationDamping * Time.deltaTime);
+		myHeight = Mathf.Lerp (myHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+		Quaternion currentRotation = Quaternion.Euler (0, myAngle, 0);
+		transform.position = Car.position;
+		transform.position -= currentRotation*Vector3.forward * distance;
+		transform.position = new Vector3(transform.position.x, myHeight, transform.position.z);
+		transform.LookAt (Car);
+	}
+
+	void FixedUpdate () {
+		localVelocity = Car.InverseTransformDirection (Car.rigidbody.velocity).z;
+		if (localVelocity < -0.5f) {
+			rotationVector = new Vector3(0, Car.eulerAngles.y + 180, 0);
+		} else {
+			rotationVector = new Vector3(0, Car.eulerAngles.y, 0);
+		}
+		float acc = Car.rigidbody.velocity.magnitude;
+		camera.fieldOfView = defaultFOV + acc*zoomRatio*Time.deltaTime;
+	}
+}
--- Assets/Scripts/WheelScript.cs
+++ Assets/Scripts/WheelScript.cs
@@ -3,22 +3,30 @@ using System.Collections;
 
 public class WheelScript : MonoBehaviour {
 
-	WheelCollider WheelLF, WheelLR, WheelRF, WheelRR;
-	float maxTorque = 50;
+	public WheelCollider WheelLF, WheelLR, WheelRF, WheelRR;
+	public Transform WheelLFTrans, WheelLRTrans, WheelRFTrans, WheelRRTrans;
+	public float maxTorque = 50;
 
 	// Use this for initialization
 	void Start () {
-		WheelLF = transform.FindChild("WheelColliders").FindChild("Wheel_LF").GetComponent<WheelCollider>();
-		WheelLR = transform.FindChild("WheelColliders").FindChild("Wheel_LR").GetComponent<WheelCollider>(); 
-		WheelRF = transform.FindChild("WheelColliders").FindChild("Wheel_RF").GetComponent<WheelCollider>(); 
-		WheelRR = transform.FindChild("WheelColliders").FindChild("Wheel_RR").GetComponent<WheelCollider>(); 
+//		WheelLF = transform.FindChild("WheelColliders").FindChild("Wheel_LF").GetComponent<WheelCollider>();
+//		WheelLR = transform.FindChild("WheelColliders").FindChild("Wheel_LR").GetComponent<WheelCollider>(); 
+//		WheelRF = transform.FindChild("WheelColliders").FindChild("Wheel_RF").GetComponent<WheelCollider>(); 
+//		WheelRR = transform.FindChild("WheelColliders").FindChild("Wheel_RR").GetComponent<WheelCollider>(); 
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void FixedUpdate () {
 		WheelRR.motorTorque = -maxTorque * Input.GetAxis ("Vertical");
 		WheelLR.motorTorque = -maxTorque * Input.GetAxis ("Vertical");
 		WheelLF.steerAngle = 10 * Input.GetAxis ("Horizontal");
 		WheelRF.steerAngle = 10 * Input.GetAxis ("Horizontal");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		WheelLFTrans.Rotate (WheelLF.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+		WheelLRTrans.Rotate (WheelLR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+		WheelRFTrans.Rotate (WheelRF.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+		WheelRRTrans.Rotate (WheelRR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
 	}
 }
